5ff24e98a0edfab5eaad71928eebac134183d29f
MrZ_26 <1046101471@qq.com>
2024-08-15 17:00:26 +0800

    添加群能量系统，词典不再限制管理可用，但非管理查词典需要花费能量

--- main.lua
+++ main.lua
@@ -1,10 +1,13 @@
+local Time=love.timer.getTime
 love._openConsole()
 --------------------------------------------------------------
 require'Zenitha'
 ZENITHA.setMaxFPS(30)
-ZENITHA.setDrawFreq(0.1)
+ZENITHA.setDrawFreq(10)
 ZENITHA.setUpdateFreq(100)
 --------------------------------------------------------------
+local initCharge=620
+local maxCharge=620
 local ws=WS.new{
     host='localhost',
     port='3001',
@@ -29,9 +32,7 @@ function Bot.receiveMessage(M)
             -- end
         else
             ---@cast M LLOneBot.Event.GroupMessage
-            if M.sender.role~='member' then
-                SearchDict(M.raw_message,M.group_id)
-            end
+            SearchDict(M.raw_message,M.group_id,nil,M.sender.role~='member')
         end
     elseif M.post_type=='notice' then
         ---@cast M LLOneBot.Event.Notice
@@ -94,31 +95,96 @@ function Bot.mainLoop() -- Coroutine!
     end
 end
 --------------------------------------------------------------
-local dictData=require'dict_zh'
-local function simpComp(a,b)
-    a=a:gsub('%s',''):lower()
-    b=b:gsub('%s',''):lower()
-    return a==b
+---@class Group
+---@field charge number
+---@field maxCharge number
+---@field lastUpdateTime number
+---@field lastHintTimeMap table<string,number>
+local Group={}
+
+---@return Group
+function Group.new()
+    local g={
+        charge=initCharge,
+        maxCharge=maxCharge,
+        lastUpdateTime=0,
+        lastHintTimeMap={},
+    }
+    setmetatable(g,{__index=Group})
+    return g
 end
-function SearchDict(word,group_id,user_id)
+function Group.updateAll(map,dt)
+    for id,g in next,map do
+        g:update(dt)
+    end
+end
+
+function Group:update()
+    self.charge=math.min(self.charge+(Time()-self.lastUpdateTime),self.maxCharge)
+    self.lastUpdateTime=Time()
+end
+function Group:canShowHint(opt,minDT)
+    self:update()
+    if Time()-(self.lastHintTimeMap[opt] or -1e99)>minDT then
+        self.lastHintTimeMap[opt]=Time()
+        return true
+    end
+end
+function Group:cost(pow)
+    self:update()
+    if self.charge>=pow then
+        self.charge=self.charge-pow
+        return true
+    else
+        return false
+    end
+end
+function Group:use(pow)
+    self:update()
+    self.charge=math.max(self.charge-pow,0)
+end
+
+---@type table<number, Group>
+local groupMap=setmetatable({},{
+    __index=function(t,id)
+        t[id]=Group.new()
+        return t[id]
+    end
+})
+
+--------------------------------------------------------------
+local dictData=require'dict_zh'
+local function simpStr(s)
+    return s:gsub('%s',''):lower()
+end
+for i=1,#dictData do
+    dictData[simpStr(dictData[i][1])]=dictData[i]
+end
+function SearchDict(word,group_id,user_id,free)
+    if not group_id then return end
     if word:sub(1,1)~='#' then return end
-    local searchWord=word:sub(2)
-    local result
-    for i=1,#dictData do
-        if simpComp(dictData[i][1],searchWord) then
-            result=dictData[i][1]..": \n"..dictData[i][2]
-            if dictData[i][3] then
-                result=result.."\n相关链接: "..dictData[i][3]
-            end
-            break
+
+    local entry=dictData[simpStr(word:sub(2))]
+    if not entry then return end
+
+    if not free and not groupMap[group_id]:cost(62) then
+        if groupMap[group_id]:canShowHint('dictPower',26) then
+            Bot.sendMessage{
+                group_id=group_id,
+                message="达到查询频率限制，每十分钟只能查十次",
+            }
         end
+        return
     end
-    if group_id then
-        Bot.sendMessage{
-            group_id=group_id,
-            message="[仅群管理可用] "..result,
-        }
+
+    local result=entry[1]..": \n"..entry[2]
+    if entry[3] then
+        result=result.."\n相关链接: "..entry[3]
     end
+    Bot.sendMessage{
+        group_id=group_id,
+        message=result,
+    }
 end
 --------------------------------------------------------------
 print('--------------------------')
@@ -136,10 +202,21 @@ else
     TASK.new(Bot.mainLoop)
 end
 --------------------------------------------------------------
+ZENITHA.globalEvent.drawCursor=NULL
+ZENITHA.globalEvent.clickFX=NULL
 local scene={}
 
 function scene.load() end
 function scene.update() end
+function scene.keyDown(k)
+    if k=='space' then
+        print('--------------------------\nGroups Info:')
+        for id,g in next,groupMap do
+            print('Group '..id..' :')
+            for key,val in next,g do print(key,val) end
+        end
+    end
+end
 function scene.draw() end
 function scene.unload() end
 
