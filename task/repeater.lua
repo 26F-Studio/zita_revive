9cb7174402dd3e3c9b0904f296a7c355ef278b73
MrZ_26 <1046101471@qq.com>
2024-08-18 08:52:10 +0800

    优化复读过滤器

--- task/repeater.lua
+++ task/repeater.lua
@@ -1,14 +1,18 @@
 local utf8=require('utf8')
+local badWords={"cq:","zita","z酱","mrz"}
 ---@type Task_raw
 return {
     func=function(S,M)
-        if S.priv or M.raw_message:find("CQ") or S:getLock('repeater') then return false end
+        if S.priv or S:getLock('repeater_silence') then return false end
+        for i=1,#badWords do if M.raw_message:lower():find(badWords[i]) then return false end end
         local len=utf8.len(M.raw_message)
-        if not (len<=26 or math.random()<=MATH.interpolate(1,.0626+math.min(S:getTimeCheckpoint('repeater')/2600,1)*.062,26,.0126,len)) then
-            S:setTimeCheckpoint('repeater')
-            S:lock('repeater',26)
-            S:send(M.raw_message)
-        end
+        if len>=26 or not S:lock('repeater_fastFilter',4.2) then return false end
+        local dt=S:getTimeCheckpoint('repeater')
+        local startRate=.0626+MATH.cLerp(0,1,dt/626)*.042
+        if MATH.roll(MATH.interpolate(1,startRate,26,.0126,len)) then return false end
+        S:setTimeCheckpoint('repeater')
+        S:lock('repeater_silence',26)
+        S:send(M.raw_message)
         return true
     end,
 }
