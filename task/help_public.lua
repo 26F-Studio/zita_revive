a9f8c9901d25be854edd5096d7e19641477b93ff
MrZ_26 <1046101471@qq.com>
2024-08-22 04:30:47 +0800

    更新help

--- task/help_public.lua
+++ task/help_public.lua
@@ -6,7 +6,13 @@ return {
         local mes=M.raw_message:lower()
         if not wordList[mes] then return false end
         if S:forceLock('help_public',62) then
-            S:send("【Zita-Revive】 小z回来啦！\n萌新有疑问时发送#[关键词]就可以查询小Z词典精简版\n词条标题前双井号表示有补充信息，紧接着发送“##”查看完整内容\n技术还处于试验阶段，随时可能停机，对不起喵！")
+            S:send(STRING.trimIndent[[
+                【Zita-Revive】 小z回来啦！
+                萌新有疑问时发送#[关键词]就可以查询小Z词典精简版
+                标题前双井号表示有补充信息，紧接着发送“##”查看
+                #游戏 查询目前大家玩的都是什么方块游戏
+                试验阶段，随时可能停机，对不起喵！
+            ]])
         else
             S:send(M.raw_message:lower()=="小z在吗" and "不在喵" or "刚刚发过了，不要刷屏谢谢喵")
         end
