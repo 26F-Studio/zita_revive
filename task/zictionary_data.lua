cfd5b9f778e75ea3ae5e5f25bbbf8b996eeb261e
MrZ_26 <1046101471@qq.com>
2026-04-20 13:25:42 +0800

    词典增加ospin链接

--- task/zictionary_data.lua
+++ task/zictionary_data.lua
@@ -341,8 +341,8 @@ local main={
     {
         word="ospin;o-spin",
         title="O-Spin",
-        text="由于O块旋转不变只能左右移所以经常被卡住，于是就有了O-Spin这个梗",
-        detail="有人做了T99/TF中的O块变形的特效视频广为流传；\n一些旋转系统允许O块旋进坑；\nTech设计的变形系统中可以旋转O来变形/传送进入一些特定形状的洞\n"..CQ.img(Config.extraData.imgPath.."ospin.gif"),
+        text="由于O块旋转不变只能左右移所以经常被卡住，于是就有了O-Spin这个梗，有人做视频有人做游戏，慢慢地也形成了一种文化",
+        link="dunspixel.github.io/ospin-guide",
     },
     {
         word="踢墙;踢墙表;旋转系统;rs;rotation system",
