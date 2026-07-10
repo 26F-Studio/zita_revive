89118924ad09d18d3b2b01325dd298bf584d41aa
MrZ_26 <1046101471@qq.com>
2026-07-11 06:42:05 +0800

    实现llm模块功能（需要在配置文件里有api key等参数）

--- task/llm.lua
+++ task/llm.lua
@@ -1,10 +1,60 @@
+local available=Config.extraData.llmModel and Config.extraData.llmSystemPrompt and Config.extraData.llmKey
+
+if not available then LOG('warn',"LLM模块需要配置3个参数") end
+
+local curlCmd=[[
+curl -s https://api.deepseek.com/chat/completions \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $1" \
+  -d '$2'
+]]
+
+local ansPool={}
+local function parseAnswer(json) return JSON.decode(json).choices[1].message.content end
+local function task_pollAnswer(S,sid)
+    local suc,res
+    repeat
+        res=ASYNC.get('llm_'..sid)
+        TASK.yieldT(.26)
+    until res
+    suc,res=pcall(parseAnswer,res)
+    S:send(suc and res or "结果解析失败："..res)
+    ansPool[sid]=nil
+end
+
 ---@type Task_raw
 return {
     message=function(S,M)
-        local msg=M.raw_message
-        if msg:match("^%[CQ:at,id="..Config.botID) then
-            S:send("喵？")
+        if not available then return false end
+        local msg=STRING.trim(M.raw_message):match("^%[CQ:at,qq="..Config.botID.."%]%s*(.*)$")
+        if not msg then return false end
+        if not Bot.isAdmin(M.user_id) then
+            if S:forceLock('llm_permission_denied',26) then S:send(Config.adminName.."才能这样做喵") end
+            return true
         end
-        return false
+
+        local data={
+            model=Config.extraData.llmModel,
+            thinking={type="disabled"},
+            reasoning_effort="high",
+            stream=false,
+            messages={
+                {role="system",content=Config.extraData.llmSystemPrompt},
+                {role="user",  content=msg},
+            },
+        }
+        local suc,res=pcall(JSON.encode,data)
+        if not suc then
+            if S:forceLock('llm_json_encode_error',26) then S:send("json打包错误："..res) end
+            return true
+        end
+
+        local sid
+        repeat sid=math.random(260) until not ansPool[sid]
+        ansPool[sid]=true
+
+        ASYNC.runCmd('llm_'..sid,STRING.repD(curlCmd,Config.extraData.llmKey,res))
+        TASK.new(task_pollAnswer,S,sid)
+        return true
     end,
 }
